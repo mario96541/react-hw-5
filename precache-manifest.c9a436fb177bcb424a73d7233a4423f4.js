c6d9b7fc59d9152a6b6b4928208833c3ecc07d12
mario96541 <41206432+mario96541@users.noreply.github.com>
2019-08-09 15:36:22 +0300

    Updates

---
+++ precache-manifest.c9a436fb177bcb424a73d7233a4423f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f58fd842c9d80a0d9d82706c04b45f6",
+    "url": "/react-hw-5/index.html"
+  },
+  {
+    "revision": "546ea3473c55874f5931",
+    "url": "/react-hw-5/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "5c00aa13fa1fc128ffc2",
+    "url": "/react-hw-5/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "546ea3473c55874f5931",
+    "url": "/react-hw-5/static/js/2.682b0888.chunk.js"
+  },
+  {
+    "revision": "5c00aa13fa1fc128ffc2",
+    "url": "/react-hw-5/static/js/main.bb79b1de.chunk.js"
+  },
+  {
+    "revision": "0f86d851f6c42f1ca5e8",
+    "url": "/react-hw-5/static/js/runtime~main.f4a34764.js"
+  }
+]);
